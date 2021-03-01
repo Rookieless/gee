b3745dd74984646b91993d83e93bd2a886032f9f
xin.liu <xin.liu@mail.hypers.com>
2021-03-01 15:34:16 +0800

    add:add context variable

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gee"
 	"net/http"
 )
@@ -9,14 +8,19 @@ import (
 func main() {
 	r := gee.New()
 
-	r.GET("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "URL.Path = %q\n", r.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(rw http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(rw, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
