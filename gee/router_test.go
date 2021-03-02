4e21a70bec4dd5827d2d1b83fff1b4eb00f9c105
xin.liu <xin.liu@mail.hypers.com>
2021-03-02 10:28:05 +0800

    add:add trie and update router

---
+++ gee/router_test.go
@@ -0,0 +1,44 @@
+package gee
+
+import (
+	"reflect"
+	"testing"
+)
+
+func newTestRouter() *router {
+	r := newRouter()
+	r.addRoute("GET", "/", nil)
+	r.addRoute("GET", "/hello/:name", nil)
+	r.addRoute("GET", "/hello/b/c", nil)
+	r.addRoute("GET", "/hi/:name", nil)
+	r.addRoute("GET", "/assets/*filepath", nil)
+	return r
+}
+
+func TestParsePattern(t *testing.T) {
+	ok := reflect.DeepEqual(parsePattern("/p:name"), []string{"p", ":name"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*"), []string{"p", "*"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*name/*"), []string{"p", "*name"})
+	if !ok {
+		t.Fatal("test parsePattern failed")
+	}
+}
+
+func TestGetRoute(t *testing.T) {
+	t := newTestRouter()
+	n, ps := r.getRoute("GET", "/hello/eric")
+
+	if n == nil {
+		t.Fatal("nil shouldn't be returned")
+	}
+
+	if n.pattern != "/hello/:name" {
+		t.Fatal("should match /hello/:name")
+	}
+
+	if ps["name"] != "eric"{
+		t.Fatal("name shoulf be equal to 'eric')
+	}
+
+	fmt.Printf("matched path: %s,params['name']: %s\n", n.pattern, ps["name"])
+}
