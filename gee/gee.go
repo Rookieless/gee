0f903f5f6f2d5844a7cd05f1450b5735c8a55769
xin.liu <xin.liu@mail.hypers.com>
2021-03-02 15:02:42 +0800

    feat:add routerGroup, can addRouter by routerGroup

--- gee/gee.go
+++ gee/gee.go
@@ -1,13 +1,46 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
+// 路由分组
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 支持中间件
+	parent      *RouterGroup  //指定上层路由分组是谁
+	engine      *Engine       // 用于访问Engine的某些属性， 所有的RouterGroup共用一个engine实例
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{prefix: group.prefix + prefix, parent: group, engine: engine}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+// 这里把Engine抽象成最顶层的路由组
 type Engine struct {
-	router *router
+	*RouterGroup // 嵌套字段
+	router       *router
+	groups       []*RouterGroup
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -16,7 +49,10 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
